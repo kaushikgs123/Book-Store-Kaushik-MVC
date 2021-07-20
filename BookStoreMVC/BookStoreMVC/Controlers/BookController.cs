60184b0a0824a0974888bef0f40a41d375f2ed7a
mmraihan <mm_raihan@live.com>
2021-07-20 22:45:48 +0600

    Controller

---
+++ BookStoreMVC/BookStoreMVC/Controlers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMVC.Controlers
+{
+    public class BookController : Controller
+    {
+       public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book with Id= {id}";
+        }
+    }
+}
