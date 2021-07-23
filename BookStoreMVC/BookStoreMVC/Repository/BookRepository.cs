5f25a3c8ab87725c763fe2c7be41b43c8b905701
mmraihan <mm_raihan@live.com>
2021-07-23 23:15:58 +0600

    Added product detail, resoloved bugs and updated library

--- BookStoreMVC/BookStoreMVC/Repository/BookRepository.cs
+++ BookStoreMVC/BookStoreMVC/Repository/BookRepository.cs
@@ -30,12 +30,41 @@ namespace BookStoreMVC.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){Id=1, Title="MVC", Author="Raihan", Description="This is a description section"},
-                new BookModel(){Id=2, Title="Web APi", Author="Zakaria", Description="This is a description section"},
-                new BookModel(){Id=3, Title="C#", Author="Tanjeeb", Description="This is a description section"},
-                new BookModel(){Id=4, Title="PHP", Author="Mridul",Description="This is a description section"},
-                new BookModel(){Id=5, Title="Python", Author="Tanvir", Description="This is a description section"},
-                new BookModel(){Id=6, Title="Data Structure", Author="Tanjeeb", Description="This is a description section"},
+                new BookModel(){Id=1, Title="MVC", Author="Raihan", Description="This is a description section sectionLorem ipsum dolor sit amet consectetur" +
+                " adipisicing elit. Ducimus dolores molestiae laborum quaerat exercitationem nesciunt odio ipsum consequuntur. Architecto doloremque natus " +
+                "aspernatur" +  " enim rem expedita at mollitia, iure nostrum laborumenim rem expedita at mollitia, iure nostrum laborum", Category="Programming",
+                Language="English", TotalPages=765},
+
+
+                new BookModel(){Id=2, Title="Web APi", Author="Zakaria", Description="This is a description section sectionLorem ipsum dolor sit amet consectetur" +
+                " adipisicing elit. Ducimus dolores molestiae laborum quaerat exercitationem nesciunt odio ipsum consequuntur. Architecto doloremque natus " +
+                "aspernatur" +  " enim rem expedita at mollitia, iure nostrum laborumenim rem expedita at mollitia, iure nostrum laborum",Category="Programming",
+                Language="English", TotalPages=865},
+
+                new BookModel(){Id=3, Title="C#", Author="Tanjeeb", Description="This is a description sectionThis is a description section sectionLorem ipsum " +
+                "dolor sit amet consectetur" +
+                " adipisicing elit. Ducimus dolores molestiae laborum quaerat exercitationem nesciunt odio ipsum consequuntur. Architecto doloremque natus " +
+                "aspernatur" +  " enim rem expedita at mollitia, iure nostrum laborumenim rem expedita at mollitia, iure nostrum laborum",Category="Programming",
+                Language="English", TotalPages=595},
+
+                new BookModel(){Id=4, Title="PHP", Author="Mridul",Description="This is a description sectionThis is a description section sectionLorem ipsum dolor" +
+                " sit amet consectetur" +
+                " adipisicing elit. Ducimus dolores molestiae laborum quaerat exercitationem nesciunt odio ipsum consequuntur. Architecto doloremque natus " +
+                "aspernatur" +  " enim rem expedita at mollitia, iure nostrum laborumenim rem expedita at mollitia, iure nostrum laborum",Category="Programming",
+                Language="English", TotalPages=788},
+
+                new BookModel(){Id=5, Title="Python", Author="Tanvir", Description="This is a description sectionThis is a description section sectionLorem ipsum" +
+                " dolor sit amet consectetur" +
+                " adipisicing elit. Ducimus dolores molestiae laborum quaerat exercitationem nesciunt odio ipsum consequuntur. Architecto doloremque natus " +
+                "aspernatur" +  " enim rem expedita at mollitia, iure nostrum laborumenim rem expedita at mollitia, iure nostrum laborum",Category="Programming",
+                Language="English", TotalPages=965},
+
+                new BookModel(){Id=6, Title="Data Structure", Author="Tanjeeb", Description="This is a description sectionThis is a description section sectionLorem " +
+                "ipsum dolor sit amet consectetur" +
+                " adipisicing elit. Ducimus dolores molestiae laborum quaerat exercitationem nesciunt odio ipsum consequuntur. Architecto doloremque natus " +
+                "aspernatur" +  " enim rem expedita at mollitia, iure nostrum laborumenim rem expedita at mollitia, iure nostrum laborum",Category="Programming",
+                Language="English", TotalPages=465},
+
 
             };
         }
