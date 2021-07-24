7868bc7469207c436eeede631b6fee60b5e85614
mmraihan <mm_raihan@live.com>
2021-07-24 21:33:56 +0600

    Anchor Tag Helper

--- BookStoreMVC/BookStoreMVC/Startup.cs
+++ BookStoreMVC/BookStoreMVC/Startup.cs
@@ -35,7 +35,12 @@ namespace BookStoreMVC
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapDefaultControllerRoute();
+            //endpoints.MapDefaultControllerRoute();
+            endpoints.MapControllerRoute(
+                name: "Default",
+                pattern: "BookApp/{controller=Home}/{action=Index}/{id?}");
+                   
+                
             });
         }
     }
