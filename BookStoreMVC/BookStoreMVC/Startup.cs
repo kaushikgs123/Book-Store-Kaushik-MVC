e492de32df94265f11e685ad8d9faca052517690
mmraihan <mm_raihan@live.com>
2021-07-25 19:33:05 +0600

    Setup DbContext Class

--- BookStoreMVC/BookStoreMVC/Startup.cs
+++ BookStoreMVC/BookStoreMVC/Startup.cs
@@ -1,6 +1,8 @@
+using BookStoreMVC.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +18,9 @@ namespace BookStoreMVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreContext>
+                (options => options.UseSqlServer("Server=. ; DataBase=BookStore; Integrated Security= True;"));
+
             services.AddControllersWithViews();
 #if DEBUG
             services.AddRazorPages().AddRazorRuntimeCompilation();  //It works only for development environment
