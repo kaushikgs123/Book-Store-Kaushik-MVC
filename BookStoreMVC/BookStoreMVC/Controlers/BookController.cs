d818cd1aaa8b939a65b1ea6b059f356745d431c2
mmraihan <mm_raihan@live.com>
2021-12-08 18:01:24 +0600

    Making Async call

--- BookStoreMVC/BookStoreMVC/Controlers/BookController.cs
+++ BookStoreMVC/BookStoreMVC/Controlers/BookController.cs
@@ -43,9 +43,9 @@ namespace BookStoreMVC.Controlers
 
         [HttpPost]
       
-        public IActionResult AddNewBook(BookModel bookModel)
+        public async Task<IActionResult> AddNewBook(BookModel bookModel)
         {
-           int id= _bookRepository.AddNewBook(bookModel);
+           int id= await _bookRepository.AddNewBook(bookModel);
             if (id>0)
             {
                 return RedirectToAction(nameof(AddNewBook),new { isSuccess=true, bookId=id});
