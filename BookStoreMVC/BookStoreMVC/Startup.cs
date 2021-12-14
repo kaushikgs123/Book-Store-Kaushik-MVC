bc93356ab4f4953ec38afff27cd62f91e1a7ddfd
mmraihan <mm_raihan@live.com>
2021-12-14 15:07:26 +0600

    Connection string

--- BookStoreMVC/BookStoreMVC/Startup.cs
+++ BookStoreMVC/BookStoreMVC/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -15,12 +16,20 @@ namespace BookStoreMVC
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           
+
             services.AddDbContext<BookStoreContext>
-                (options => options.UseSqlServer("Server=DESKTOP-4BMDG8B; DataBase=BookStrore; Integrated Security= True;"));
+                (options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews();
 #if DEBUG
