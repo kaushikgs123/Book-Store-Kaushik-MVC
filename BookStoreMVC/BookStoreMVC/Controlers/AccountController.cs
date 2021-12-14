a46d4b60312c979b67382ba606a982fa79ab69e0
mmraihan <mm_raihan@live.com>
2021-12-14 18:13:17 +0600

    Design a registration (Signup) page

---
+++ BookStoreMVC/BookStoreMVC/Controlers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using BookStoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMVC.Controlers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult SignUp(SignUpUserModel userModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ BookStoreMVC/BookStoreMVC/Models/SignUpUserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMVC.Models
+{
+    public class SignUpUserModel
+    {
+        [Required(ErrorMessage ="Please enter your email")]
+        [Display(Name ="Email Address")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please entera strong password")]
+        [Compare("ConfirmPassword", ErrorMessage ="Password doesn't match")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+
+        public string ConfirmPassword { get; set; }
+
+    }
+}
